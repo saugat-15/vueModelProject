af326fed2289f4c7cc4d5cd9fae7201a6d044dff
Prasanga Shrestha <saugatgiri15@gmail.com>
2022-06-24 22:48:36 +1000

    upto emitting comps tuts finished

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModel">
+    <Model :header="header" :text="text"  theme="sale" @close="toggleModel" />
+  </div>
+  <p>Welcome...</p>
+  <button @click="toggleModel">Open Model</button>
+  
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Model from "./components/Model.vue";
+
+export default {
+  name: "App",
+  component: { Model },
+  data() {
+    return {
+      title: "My first vue App :)",
+      header: "Sign up for the giveaway!",
+      text: "Hurry Up!",
+      showModel: false
+    };
+  },
+  methods: {
+    handleClick() {
+      let ref = this.$refs.name;
+      ref.style.display = "inline-block";
+      ref.classList.add("active");
+      ref.focus();
+    },
+    toggleModel() {
+      this.showModel = !this.showModel;
+    }
+  },
+  components: { Model },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid rgb(156, 155, 155);
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Model.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="backdrop" ref="backdrop" @click="closeModel">
+    <div class="model" :class="{ sale: theme === 'sale' }">
+      <!-- <i class="fa fa-times" aria-hidden="true" @click="handleHide"></i> -->
+
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+      closeModel() {
+        this.$emit('close')
+    },
+  },
+};
+</script>
+
+<style>
+.model {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.model h1 {
+  color: rgb(15, 226, 163);
+  border-style: none;
+  padding: 0;
+}
+
+.model i {
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+}
+
+.model.sale {
+  background: crimson;
+  color: #fff;
+}
+
+.model.sale h1 {
+  color: white;
+}
+</style>
